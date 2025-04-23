40d03301ccd1cc03f93c03b90a4e85febb8e4942
prince prasad <pp1614582@gmail.com>
2025-04-23 20:03:45 +0530

    commit 4

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,18 @@ const App = () => {
   return (
     <div>
       <ToolTip text="This is a tooltip">
-        <h2>Hover over me</h2>
+        <h2 className="tooltip">
+          Hover over me
+         
+          </h2>
 
       </ToolTip>
 
       <ToolTip text="This is another tooltip">
-      <p>Hover over me to see another tooltip</p>
+        <p className="tooltip">
+        Hover over me to see another tooltip
+          
+         </p>
       </ToolTip>
     </div>
   )
