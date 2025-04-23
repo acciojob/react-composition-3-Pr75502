c3ed827b24bd612163b597461c0973de64ca9acb
prince prasad <pp1614582@gmail.com>
2025-04-23 19:05:06 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import ToolTip from "./Tooltip";
+
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ToolTip text="This is a tooltip">
+        <h1>Hover over me (h1)</h1>
+
+      </ToolTip>
+
+      <ToolTip text="This is another tooltip">
+      <p>Hover over me to see another tooltip (p)</p>
+      </ToolTip>
     </div>
   )
 }
